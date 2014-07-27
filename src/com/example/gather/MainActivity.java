9d80a3a68aa9cf2de6e0fcf76736b97448ebd915
Mukund Chillakanti <mukundc@berkeley.edu>
2014-07-26 21:17:27 -0700

    working with Facebook

--- src/com/example/gather/MainActivity.java
+++ src/com/example/gather/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.gather;
 
+import com.facebook.*;
+import com.facebook.model.*;
+import android.widget.TextView;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,8 +15,37 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// start Facebook Login
+		Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+		    // callback when session changes state
+		    @Override
+		    public void call(Session session, SessionState state, Exception exception) {
+		    	if (session.isOpened()) {
+		    		// make request to the /me API
+		    		Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+		    		  // callback after Graph API response with user object
+		    		  @Override
+		    		  public void onCompleted(GraphUser user, Response response) {
+		    			  if (user != null) {
+		    				  TextView welcome = (TextView) findViewById(R.id.welcome);
+		    				  welcome.setText("Hello " + user.getName() + "!");
+		    				}
+		    		  }
+		    		}).executeAsync();
+		    		}
+		    }
+		});
 	}
 
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	  super.onActivityResult(requestCode, resultCode, data);
+	  Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
